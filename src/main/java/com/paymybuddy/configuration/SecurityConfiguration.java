cc523a83fc66dab8c9109cdc70afedaed4d566e9
matsamb <matondo.lusamba@gmail.com>
2022-05-12 16:37:57 +0200

    sql script done

--- src/main/java/com/paymybuddy/configuration/SecurityConfiguration.java
+++ src/main/java/com/paymybuddy/configuration/SecurityConfiguration.java
@@ -1,5 +1,46 @@
 package com.paymybuddy.configuration;
 
-public class SecurityConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(HttpSecurity httpSecurity) throws Exception{
+		
+		httpSecurity
+		.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasRole("USER")
+		.anyRequest().authenticated()
+		.and()
+		.formLogin();
+		
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+		
+		authenticationManagerBuilder.inMemoryAuthentication()
+			.withUser("user").password(passwordEncoder().encode("u123")).roles("USER")
+			.and()
+			.withUser("admin").password(passwordEncoder().encode("a123")).roles("ADMIN");
+		
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,56 @@
+DROP database prodPaymybuddy;
+
+CREATE DATABASE IF NOT EXISTS prodPaymybuddy;
+
+USE prodPaymybuddy;
+
+CREATE TABLE IF NOT EXISTS utilisateur(
+	email VARCHAR (50) NOT NULL,
+	first_name VARCHAR (50),
+	last_name VARCHAR (50),
+	balance FLOAT,
+	password VARCHAR (50) NOT NULL,
+	PRIMARY KEY (email)
+);
+
+CREATE TABLE IF NOT EXISTS connection(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	fk_payer_email VARCHAR (50) NOT NULL,
+	fk_payee_email VARCHAR (50) NOT NULL,
+	
+	PRIMARY KEY (id),
+	/*INDEX utilisateur_connection_fk, 
+	INDEX utilisateur_connection_fk, */
+	
+	FOREIGN KEY (fk_payer_email) REFERENCES utilisateur (email) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (fk_payee_email) REFERENCES utilisateur (email) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS payment(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	fk_connection_id INTEGER NOT NULL,
+	amount FLOAT,
+	payment_date DATE,
+	PRIMARY KEY (id),
+	/*INDEX connection_payment_fk, */
+	FOREIGN KEY (fk_connection_id) REFERENCES connection (id)
+);
+
+CREATE TABLE IF NOT EXISTS bank_account(
+	iban INTEGER NOT NULL AUTO_INCREMENT,
+	fk_utilisateur_email VARCHAR (50) NOT NULL,
+	balance FLOAT,
+	PRIMARY KEY (iban),
+	/*INDEX utilisateur_bank_account_fk,*/
+	FOREIGN KEY (fk_utilisateur_email) REFERENCES utilisateur (email) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS transaction(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	fk_iban INTEGER NOT NULL,
+	amount FLOAT,
+	transaction_date DATE,
+	PRIMARY KEY (id),
+	/*INDEX bank_account_transaction_fk,*/
+	FOREIGN KEY (fk_iban) REFERENCES bank_account (iban)
+);
