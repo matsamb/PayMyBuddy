ae38bb3c3c1cc59f8652486a14ff322ababa53bc
matsamb <matondo.lusamba@gmail.com>
2022-06-03 12:29:22 +0200

    bank controller ok

--- src/main/java/com/paymybuddy/controller/BankTransactionController.java
+++ src/main/java/com/paymybuddy/controller/BankTransactionController.java
@@ -1,5 +1,29 @@
 package com.paymybuddy.controller;
 
+import javax.annotation.security.RolesAllowed;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.paymybuddy.dto.ViewPayment;
+
+@RestController
+@RolesAllowed("USER")
 public class BankTransactionController {
 
+	@GetMapping("/banktransaction")//?iban=<iban>
+	public Iterable<ViewPayment> getBankTransaction(@RequestParam String iban){
+	
+		return null;
+		
+	}
+	
+	@PostMapping("/banktransaction")
+	public Iterable<ViewPayment> addBankTransaction(){
+		return null;
+		
+	}
+	
 }
